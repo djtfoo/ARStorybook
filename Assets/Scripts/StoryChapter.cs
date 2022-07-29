2f536b4cb4b2a467a0bfc267a406df36898f1fe2
JT <djt.foo@gmail.com>
2022-07-29 11:10:08 +0800

    first commit

---
+++ Assets/Scripts/StoryChapter.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryChapter : MonoBehaviour {
+
+    private Animator animator = null;
+
+	// Use this for initialization
+	void Start () {
+        animator = gameObject.GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public IEnumerator ActivateStoryChapter()
+    {
+        float scale = 0f;
+        gameObject.SetActive(true);
+        if (animator)
+            animator.enabled = false;
+
+        while (scale < 1f)
+        {
+            scale += Time.deltaTime;
+            if (scale > 1f)
+                scale = 1f;
+
+            transform.localScale = new Vector3(scale, scale, scale);
+            yield return null;
+        }
+
+        if (animator)
+            animator.enabled = true;
+    }
+
+    public IEnumerator DeactivateStoryChapter()
+    {
+        float scale = 1f;
+
+        while (scale > 0f)
+        {
+            scale -= Time.deltaTime;
+            if (scale < 0f)
+                scale = 0f;
+
+            transform.localScale = new Vector3(scale, scale, scale);
+            yield return null;
+        }
+
+        gameObject.SetActive(false);
+
+        if (animator)
+            animator.enabled = false;
+    }
+
+    //public IEnumerator ScaleObject(bool up)
+    //{
+    //    float scale = transform.localScale.x;
+    //    gameObject.SetActive(true);
+    //
+    //    if (up)
+    //    {
+    //        while (scale < 1f)
+    //        {
+    //            scale += Time.deltaTime;
+    //            if (scale > 1f)
+    //                scale = 1f;
+    //
+    //            transform.localScale = new Vector3(scale, scale, scale);
+    //            yield return null;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        while (scale > 0f)
+    //        {
+    //            scale -= Time.deltaTime;
+    //            if (scale < 0f)
+    //                scale = 0f;
+    //
+    //            transform.localScale = new Vector3(scale, scale, scale);
+    //            yield return null;
+    //        }
+    //
+    //        gameObject.SetActive(false);
+    //    }
+    //}
+
+}
---
+++ Assets/Scripts/StoryTransition.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryTransition : MonoBehaviour {
+
+    public StoryChapter[] chapters;
+    public Animator pageFlipAnimation;
+
+    private int currChapter = -1;
+
+	// Use this for initialization
+	void Start () {
+
+        pageFlipAnimation.gameObject.SetActive(false);
+        ResetStory();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NextChapter()
+    {
+        StartCoroutine(LoadNextChapter());
+    }
+
+    public void ResetStory()
+    {
+        currChapter = -1;
+        foreach (StoryChapter chapter in chapters)
+            chapter.gameObject.SetActive(false);
+    }
+
+    public IEnumerator LoadNextChapter()
+    {
+        pageFlipAnimation.gameObject.SetActive(true);
+
+        if (currChapter > -1)
+            yield return chapters[currChapter].DeactivateStoryChapter();
+
+        // page flip animation is playing
+        if (pageFlipAnimation.GetCurrentAnimatorStateInfo(0).length > pageFlipAnimation.GetCurrentAnimatorStateInfo(0).normalizedTime)
+            yield return null;
+
+        currChapter++;
+        if (currChapter >= chapters.Length)
+            ResetStory();
+        else
+            yield return chapters[currChapter].ActivateStoryChapter();
+    }
+
+}
